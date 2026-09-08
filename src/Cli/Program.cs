1816c6a89523ff9e14e582ccd485b9d62186b53b
Зореслава bc2025-6 <wesa06189@gmail.com>
2026-09-08 19:19:36 +0300

    lab01: solution CrossApp, Cli, домен Бібліотека

---
+++ src/Cli/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("CrossApp - практикум з крос-платформного програмування");
+Console.WriteLine("Студентка: Березюк Зореслава, група ФЕІ-36");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine($"ОС (OSDescription): {RuntimeInformation.OSDescription}");
+Console.WriteLine($"ОС (Environment)  : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу: {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
+Console.WriteLine($"Runtime           : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку: {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог  : {Environment.CurrentDirectory}");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine("Предметна область: Бібліотека (видання, примірники, читачі, видачі)");
